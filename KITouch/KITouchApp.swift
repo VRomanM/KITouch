f032fc8d3d68f3ec2c8f577a7ee3952cf147c921
VRM <vromanm@mail.ru>
2025-05-28 16:59:42 +0300

    Initial Commit

---
+++ KITouch/KITouchApp.swift
@@ -0,0 +1,17 @@
+//
+//  KITouchApp.swift
+//  KITouch
+//
+//  Created by Роман Вертячих on 28.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct KITouchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
